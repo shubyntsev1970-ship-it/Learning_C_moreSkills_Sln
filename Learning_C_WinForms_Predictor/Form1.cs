57a6bd30b362aaf5a9e7d6c1bc1a5c8c4076c1ae
shubyntsev-dev <shubyntsev1970@gmail.com>
2026-05-06 02:30:03 +0300

    Добавьте файлы проекта.

---
+++ Learning_C_WinForms_Predictor/Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+
+namespace Learning_C_WinForms_Predictor
+{
+    public partial class Form1 : Form
+    {
+        const string APP_NAME = "ULTIMATE_PREDICTOR";
+        readonly string PREDICTIONS_CONFIG_PATH = $"{Environment.CurrentDirectory}\\predictionsConfig.json";
+        private string[] _predictions;
+        private Random _random = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void bPredict_Click(object sender, EventArgs e)
+        {
+            bPredict.Enabled = false;
+
+            await Task.Run(() =>
+            {
+                for (int i = 1; i <= 100; i++)
+                {
+                    this.Invoke(new Action(() =>
+                    {
+                        UpdateProgressBar(i);
+                        this.Text = $"{APP_NAME} - {i}%";
+                    }));
+                    Thread.Sleep(5);
+                }
+            });
+
+            var index = _random.Next(_predictions.Length);
+
+            var prediction = _predictions[index];
+
+            MessageBox.Show($"{prediction} !!!");
+
+            progressBar1.Value = 0;
+            this.Text = APP_NAME;
+            bPredict.Enabled = true;
+        }
+
+        private void UpdateProgressBar(int i)
+        {
+            // To get around the progressive animation, we need to move the
+            // progress bar backwards.
+            if (i == progressBar1.Maximum)
+            {
+                // Special case as value can't be set greater than Maximum.
+                progressBar1.Maximum = i + 1;   // Temporarily Increase Maximum
+                progressBar1.Value = i + 1;     // Move past
+                progressBar1.Maximum = i;       // Reset maximum
+            }
+            else
+            {
+                progressBar1.Value = i + 1;     // Move past
+            }
+
+            progressBar1.Value = i;             // Move to correct value
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Text = APP_NAME;
+
+            try
+            {
+                var data = File.ReadAllText(PREDICTIONS_CONFIG_PATH);
+                _predictions = JsonConvert.DeserializeObject<string[]>(data);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                if (_predictions == null)
+                {
+                    Close();
+                }
+                else if (_predictions.Length == 0)
+                {
+                    MessageBox.Show("Предсказания закончились, кина не будет! =)");
+                    Close();
+                }
+            }
+        }
+    }
+}
---
+++ Learning_C_WinForms_Predictor/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанных со сборкой.
+[assembly: AssemblyTitle("Learning_C_WinForms_Predictor")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Learning_C_WinForms_Predictor")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, следует установить атрибут ComVisible в TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("f1ea0f7e-c8b9-45a4-9e9c-4abcdf9f05e8")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Редакция
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
